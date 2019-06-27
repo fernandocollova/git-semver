1b0c89b00984deedee4387f78bceb395ab7e3e6e
Fernando Collova <fernandocollova@gmail.com>
2019-06-27 13:09:33 -0300

    Improved branch checkuot

--- plugins/91-major_and_minor_branch.sh
+++ plugins/91-major_and_minor_branch.sh
@@ -8,14 +8,14 @@ function run() {
     minor_number=$(echo "$version_new" | cut --delimiter="." --fields=2)
     major_number=$(echo "$version_new" | cut --delimiter="." --fields=1)
 
-    git checkout -b "$major_number.$minor_number"
+    git checkout "$major_number.$minor_number" || git checkout -b "$major_number.$minor_number"
     if ! git merge --ff-only -m "Atomatic merge to minor branch of tag" "$current_branch"
     then
         echo "Minor tag branch cannot be fast forwarded. Aborting."
         return 113
     fi
 
-    git checkout -b "$major_number"
+    git checkout "$major_number" || git checkout -b "$major_number"
     if ! git merge --ff-only -m "Atomatic merge to major branch of tag" "$current_branch"
     then
         echo "Major tag branch cannot be fast forwarded. Aborting."
